c4d091dd2016b89eb56360db22a3679da5b6186a
victormora1 <victorjmora1@gmail.com>
2023-05-30 16:27:31 -0400

    added code and function to add searched city to history

--- script.js
+++ script.js
@@ -18,3 +18,16 @@ searchBtnEl.addEventListener('click', async function () {
     getForecastItems(forecastWeatherInfo);  
     historyAdd(cityName); // Add searched city to history
 });
+
+// Function to add searched city to history
+async function historyAdd(cityName) {
+    // Get geographic data for city
+    var geoUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${API_KEY}`;
+    var response = await fetch(geoUrl);
+    var [data] = await response.json();
+    var { name } = data;
+
+    var historyEl = document.querySelector('#historyID ul');
+    var buttonEl = document.createElement('button');
+    buttonEl.textContent = name;
+    historyEl.appendChild(buttonEl);
